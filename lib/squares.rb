257d80571ea7ac10941f4141ece565ae1ec219fc
Talal Khodr <Talal.Khodr@Gmail.com>
2014-07-15 10:06:46 +0100

    15/July/2014 Trying to merge repository with Development

---
+++ lib/squares.rb
@@ -0,0 +1,4 @@
+class Squares
+
+end
+
---
+++ spec/squares_spec.rb
@@ -0,0 +1,37 @@
+require 'squares'
+
+describe Squares do
+
+	context 'Knows what is on it' do
+
+		it 'knows if there is a ship on it' do
+
+		end
+
+	# 	it 'knows which ship is on it' do
+			
+	# 	end
+
+	# 	it 'knows if it has been hit' do
+			
+	# 	end
+
+	# end
+
+	# context 'limitations of the square' do
+		
+	# 	it 'does not accept overlapping ships' do
+			
+	# 	end
+
+	# end
+
+	# context 'reports status' do
+		
+	# 	it 'reports hits to ship' do
+			
+	# 	end
+
+	end
+
+end
